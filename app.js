f3c451a8ebef3249e9e8d49806fa58bfad9c3427
kenneth palermo <kennethross@aspevo.com>
2020-11-18 20:32:41 +0800

    Changes on the mongoose connect. taken out the monitor for the db

--- app.js
+++ app.js
@@ -61,7 +61,9 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true'
+    'mongodb://localhost/myapp',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    // 'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/messages?retryWrites=true'
   )
   .then(result => {
     app.listen(8080);
